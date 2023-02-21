4fcfb4133d44b843a6dc6a7eda47b636a0c9478f
Joanna Chang <joanna.changc@gmail.com>
2023-02-21 16:41:51 +0000

    run sims for decorr networks

--- simulation/run.sh
+++ simulation/run.sh
@@ -1,19 +1,25 @@
 # !/bin/bash
 # Script to run simulations
 export seed_str='0 1 2 3 4 5 6 7 8 9'
+export seed_str='0'
 export sds=($seed_str)
 export nseeds=${#sds[@]}
-export threads=2
+export threads=1
 export incr=$((nseeds / threads))
 
 for ((i=0;i<$threads;i++)); 
 do
     export i=$i
-    'screen -dmS run$i bash -c \
-    seeds=($seed_str)
+    screen -dmS run$i bash -c \
+    'seeds=($seed_str)
     for seed in ${seeds[@]:$((i*incr)):$(((i+1)*incr))}
     do  
-        #simulate standard networks
-        python ./run_pipeline.py 100006$seed 1 -c base_config.yaml -file Chewie_CO_CS_2016-10-14
+        # #simulate standard networks
+        # python ./run_pipeline.py 100006$seed 1 -c standard_config.yaml -file Chewie_CO_CS_2016-10-14
+
+        #simulate decorrelated networks
+        python ./run_pipeline.py 100006$seed 1 -c decorrelated_config.yaml -file Chewie_CO_CS_2016-10-14
     done'
 done
+
+
